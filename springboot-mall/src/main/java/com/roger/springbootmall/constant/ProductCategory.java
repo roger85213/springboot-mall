ec21c489022e7c19b505bbc801c435c5accf9803
roger85213 <roger880419242458@gmail.com>
2023-04-21 22:50:05 +0800

    商品功能-實作查詢商品

---
+++ springboot-mall/src/main/java/com/roger/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.roger.springbootmall.constant;
+
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    E_Book
+}
--- springboot-mall/src/main/java/com/roger/springbootmall/rowmapper/ProductRowMapper.java
+++ springboot-mall/src/main/java/com/roger/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.roger.springbootmall.rowmapper;
 
+import com.roger.springbootmall.constant.ProductCategory;
 import com.roger.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -13,7 +14,14 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+
+        String categoryStr = resultSet.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
+        //product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
+
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
